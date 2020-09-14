3fefdeaed58663a5725d08d0511e1bafc1a8a47d
Vladyslav <45554407+web-bond@users.noreply.github.com>
2020-09-14 10:26:14 +0300

    update seo url widget
    update seo url widget

--- src/widgets/ListWidget.php
+++ src/widgets/ListWidget.php
@@ -25,9 +25,16 @@ class ListWidget extends Widget{
 
         //Создаем массив ссылок всех языков с соответствующими GET параметрами
         $array_lang = [];
-        foreach (Yii::$app->getModule('languages')->languages as $key => $value){
+        
+        $module = Yii::$app->getModule('languages');
+        $default_language = $module->default_language;
+        $curentLang = Yii::$app->language;
+        
+        foreach ($module->languages as $key => $value){
+            $url = $this->update_url($value, $default_language, $curentLang);
+            $link = $this->createLink_2($key, $value, $url);
 
-            $link = $this->createLink($key, $value);
+//            $link = $this->createLink($key, $value);
             $array_lang += [$value => $link];
         }
 
@@ -44,6 +51,39 @@ class ListWidget extends Widget{
      */
     protected function createLink($key, $value){
         return Html::a($key, ['languages/default/index', 'lang' => $value], ['class' => 'language '.$value] );
+    }
+    
+    
+    protected function createLink_2($key, $value, $link){
+        return Html::a($key, $link, ['class' => 'language '.$value] );
+    }
+    protected function update_url($lang, $default_lang, $current_lang) {
+            $_lang_url = Yii::$app->request->url; //полный URL
+            $url = $_lang_url;
+            $url_list = explode('/', $_lang_url);
+            $lang_url = isset($url_list[1]) ? $url_list[1] : null;
+
+            $module = Yii::$app->getModule('languages');
+            foreach ($module->languages as $key => $value){
+                $url_list_new = $url_list;
+                if ($lang_url && $lang_url == $value) {
+                    unset($url_list_new[1]);
+                    $url = implode('/', $url_list_new);
+                }
+            }
+
+            if ($lang != $default_lang) {
+                $url = '/'.$lang.$url;
+            }
+            if ($url && $url !== '/') {
+                $url = rtrim($url, '/\\');
+            }
+            if (!$url) {
+                $url = '/';
+            }
+
+
+        return $url;
     }
 
 
